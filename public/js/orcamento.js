38a94bd453879a4ef2e677a368b3aa76893a2890
Luis Peixoto <lmpeixoto@gmail.com>
2020-05-04 23:58:51 +0100

    reformatting and using module design pattern

--- public/js/orcamento.js
+++ public/js/orcamento.js
@@ -1,87 +1,98 @@
-const fetchFormasFarmaceuticas = async () => {
-  let response = await fetch("/formasFarmaceuticas");
-  let data = await response.json();
-  return data;
-};
+const UICtrl = (function () {
 
-const fetchFatores = async () => {
-  let response = await fetch("/fatores");
-  let data = await response.json();
-  return data;
-};
+  return {
 
-const formaFarmaceuticaSelect = document.getElementById(
-  "select-forma-farmaceutica"
-);
-const fatorSelect = document.getElementById("select-fator");
-const addMatPrimaButton = document.getElementById("add-mat-prima-button");
-const removeMatPrimaButton = document.querySelector(".rem-mat-prima-button");
-const matPrimasSummaryList = document.getElementById("mat-primas-summary-list");
+    fetchFormasFarmaceuticas: async () => {
+      let response = await fetch("/formasFarmaceuticas");
+      let data = await response.json();
+      return data;
+    },
 
+    fetchFatores: async () => {
+      let response = await fetch("/fatores");
+      let data = await response.json();
+      return data;
+    },
 
-const materiasPrimas = [];
+    formaFarmaceuticaSelectPopulate: (fFarm) => {
+      const formaFarmaceuticaSelect = document.getElementById("select-forma-farmaceutica");
+      let formasFarmaceuticas = Object.keys(fFarm);
+      formasFarmaceuticas.forEach((ff) => {
+        let option = document.createElement("option");
+        option.value = ff;
+        option.text = ff;
+        formaFarmaceuticaSelect.add(option);
+      });
+    },
 
-class MateriaPrima {
-  constructor(nome, preco, qtd, fator) {
+    fatorSelectPopulate: (fatores) => {
+      const fatorSelect = document.getElementById("select-fator");
+      for (let fator in fatores) {
+        let option = document.createElement("option");
+        option.value = fator;
+        option.text = fator;
+        fatorSelect.add(option);
+      }
+    },
+  };
+})();
+
+const ItemCtrl = (function () {
+
+  const MateriaPrima = function (nome, preco, qtd, fator) {
     this.nome = nome;
     this.preco = preco;
     this.qtd = qtd;
     this.fator = fator;
-  }
-}
+  };
 
-addMatPrima = () => {
-  nome = document.getElementById('nome-mat-prim').value;
-  preco = document.getElementById('preco-mat-prim').value;
-  qtd = document.getElementById('qtd-mat-prim').value;
-  fator = document.getElementById('select-fator').value;
-  matPrima = new MateriaPrima(nome, preco, qtd, fator);
-  materiasPrimas.push(matPrima);
-  index = materiasPrimas.indexOf(matPrima);
-  console.log(matPrima);
-  let html = `
-                    <li>${matPrima.nome} <button type="button" class="rem-mat-prima-button" id="mat-prima-element-${index}><span class="glyphicon glyphicon-minus"></span>Remover</button>
-    
-                `;
-  matPrimasSummaryList.innerHTML = html;
-};
+  const materiasPrimas = [];
 
+  return {
 
-removeMatPrima = (e) => {
-    console.log(e);
-    
-}
+    addMatPrima: () => {
+      nome = document.getElementById("nome-mat-prim").value;
+      preco = document.getElementById("preco-mat-prim").value;
+      qtd = document.getElementById("qtd-mat-prim").value;
+      fator = document.getElementById("select-fator").value;
 
-addMatPrimaButton.addEventListener("click", addMatPrima);
-removeMatPrimaButton.addEventListener("click", removeMatPrima);
+      matPrima = new MateriaPrima(nome, preco, qtd, fator);
+      materiasPrimas.push(matPrima);
+      index = materiasPrimas.indexOf(matPrima);
+      console.log(matPrima);
+      let html = `<li>${matPrima.nome} <button type="button" class="rem-mat-prima-button" id="mat-prima-element-${index}><span class="glyphicon glyphicon-minus"></span>Remover</button>`;
+      const matPrimasSummaryList = document.getElementById("mat-primas-summary-list");
+      matPrimasSummaryList.innerHTML = html;
+      const removeMatPrimaButton = document.querySelector(".rem-mat-prima-button");
+      removeMatPrimaButton.addEventListener("click", ItemCtrl.removeMatPrima);
+    },
 
-formaFarmaceuticaSelectPopulate = (fFarm) => {
-  let formasFarmaceuticas = Object.keys(fFarm);
-  formasFarmaceuticas.forEach((ff) => {
-    let option = document.createElement("option");
-    option.value = ff;
-    option.text = ff;
-    formaFarmaceuticaSelect.add(option);
-  });
-};
+    removeMatPrima: () => {
 
-fatorSelectPopulate = (fatores) => {
-  for (let fator in fatores) {
-    let option = document.createElement("option");
-    option.value = fator;
-    option.text = fator;
-    fatorSelect.add(option);
-  }
-};
+    },
+  };
+})();
 
-fetchFormasFarmaceuticas().then((result) =>
-  formaFarmaceuticaSelectPopulate(result)
-);
-fetchFatores().then((result) => fatorSelectPopulate(result));
+const AppCtrl = (function (UICtrl, ItemCtrl) {
 
-// nome
-// preço/unidade
-// quantidade
-// fator de peso
+  const loadEventListeners = function () {
+    const addMatPrimaButton = document.getElementById("add-mat-prima-button");
+    addMatPrimaButton.addEventListener("click", ItemCtrl.addMatPrima);
+  };
 
-// mat de embalagem
+  const fetchData = function () {
+    let fFarm = UICtrl.fetchFormasFarmaceuticas().then((result) => UICtrl.formaFarmaceuticaSelectPopulate(result));
+    UICtrl.formaFarmaceuticaSelectPopulate(fFarm);
+    let fatores = UICtrl.fetchFatores().then((result) => UICtrl.fatorSelectPopulate(result));
+    UICtrl.fatorSelectPopulate(fatores);
+  };
+
+  return {
+    init: function () {
+      fetchData();
+      loadEventListeners();
+    },
+  };
+})(UICtrl, ItemCtrl);
+
+AppCtrl.init();
