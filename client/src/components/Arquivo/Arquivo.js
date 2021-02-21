ccee47b80abf15d2c5c166f79dabf6f8883c4c1e
Luis Peixoto <lmpeixoto@gmail.com>
2021-02-21 09:03:54 +0000

    arquivo component working

--- client/src/components/Arquivo/Arquivo.js
+++ client/src/components/Arquivo/Arquivo.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -6,24 +7,52 @@ import Grid from '@material-ui/core/Grid';
 
 import { getOrcamentoAll } from '../../utils/api';
 
+const useStyles = makeStyles((theme) => ({
+    gridContainer: {
+        marginTop: '2rem'
+    },
+    cards: {
+        margin: '1rem 0.5rem'
+    }
+}));
+
 const Arquivo = () => {
+    const classes = useStyles();
+
     const [orcamentos, setOrcamentos] = useState([]);
 
     useEffect(() => {
         const getOrcamentos = async () => {
             const orcamentosData = await getOrcamentoAll();
             setOrcamentos(orcamentosData);
-            console.log('done');
         };
-        return () => {
-            getOrcamentos();
-        };
+
+        getOrcamentos();
     }, []);
 
     return (
-        <Grid container direction={'column'} spacing={1}>
+        <Grid
+            className={classes.gridContainer}
+            container
+            direction={'column'}
+            spacing={1}
+        >
+            <Grid item>
+                <Typography variant="h4">Arquivo:</Typography>
+            </Grid>
             {orcamentos.map((orcamento) => {
-                return <p>orcamento</p>;
+                return (
+                    <Card key={orcamento._id} className={classes.cards}>
+                        <CardContent>
+                            <Typography variant="h5" component="h2">
+                                <span>Nome:</span> {orcamento.nomeManipulado}
+                            </Typography>
+                            <Typography color="textSecondary" gutterBottom>
+                                <span>Preço:</span> {orcamento.totalPrice}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                );
             })}
         </Grid>
     );
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -6,36 +6,50 @@ const URL_GET_ORCAMENTOS = 'http://localhost:5000/orcamentos/all';
 export const FATOR_F = 5.03;
 
 export const fetchFatores = async () => {
-    const fatoresResponse = await fetch(URL_FATORES);
-    const fatoresData = await fatoresResponse.json();
-    return fatoresData;
+    try {
+        const fatoresResponse = await fetch(URL_FATORES);
+        const fatoresData = await fatoresResponse.json();
+        return fatoresData;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const fetchFormasFarmaceuticas = async () => {
-    const formasFarmaceuticasResponse = await fetch(URL_FORMAS_FARMACEUTICAS);
-    const formasFarmaceuticasData = await formasFarmaceuticasResponse.json();
-    return formasFarmaceuticasData;
+    try {
+        const formasFarmaceuticasResponse = await fetch(
+            URL_FORMAS_FARMACEUTICAS
+        );
+        const formasFarmaceuticasData = await formasFarmaceuticasResponse.json();
+        return formasFarmaceuticasData;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const postOrcamento = async (dataBody) => {
-    const response = await fetch(URL_POST_NOVO_ORCAMENTO, {
-        method: 'POST',
-        body: JSON.stringify(dataBody),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    const data = await response.json();
-    console.log(data);
+    try {
+        const response = await fetch(URL_POST_NOVO_ORCAMENTO, {
+            method: 'POST',
+            body: JSON.stringify(dataBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const getOrcamentoAll = async () => {
-    const response = await fetch(URL_GET_ORCAMENTOS, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    const data = await response.json();
-    console.log('done');
+    try {
+        const response = await fetch(URL_GET_ORCAMENTOS);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 };
